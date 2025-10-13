a3868cfd44b780d99b020001f97724279e964518
Luke Sandberg <lukeisandberg@gmail.com>
2025-10-12 22:42:50 -0700

    introduce an option to use the whole app module graph in development

--- packages/next/src/server/config-schema.ts
+++ packages/next/src/server/config-schema.ts
@@ -308,6 +308,7 @@ export const experimentalSchema = {
   turbopackUseBuiltinBabel: z.boolean().optional(),
   turbopackUseBuiltinSass: z.boolean().optional(),
   turbopackModuleIds: z.enum(['named', 'deterministic']).optional(),
+  turbopackUseWholeAppModuleGraphInDev: z.boolean().optional(),
   optimizePackageImports: z.array(z.string()).optional(),
   optimizeServerReact: z.boolean().optional(),
   clientTraceMetadata: z.array(z.string()).optional(),
